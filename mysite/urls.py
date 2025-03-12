8fd905603f3c112276ab6e1d71e95fb99520e213
jk-me <36418258+jk-me@users.noreply.github.com>
2025-03-12 17:47:09 -0400

    add polls view and route 'write your first view'

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    # include assumes handling routes specifically and doesnt show default root page
+    path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
 ]
