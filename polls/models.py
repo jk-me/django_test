6a2d2050712b0e6e2e7ca0cfa289b4dff10fea1f
jk-me <36418258+jk-me@users.noreply.github.com>
2025-03-13 15:00:38 -0400

    add models, activate app and make mgirations

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+# models build on subclasses of models import
+
+# question_text and pub_date for ex. will be column names
+# Field classes for data type, ForeignKey class for relationships
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
