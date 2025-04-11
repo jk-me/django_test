908459a5d1ebd7525686f5dd0319c74365b84da1
jk-me <36418258+jk-me@users.noreply.github.com>
2025-04-11 17:19:46 -0400

    add Choice as an inline for question so a q can be edited with its choices

--- polls/admin.py
+++ polls/admin.py
@@ -3,12 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from .models import  Choice, Question
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3 # number of empty choice fields to display
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"]}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice) # choice form adds a single choice and allows choosing existing/adding a new question to attach to
